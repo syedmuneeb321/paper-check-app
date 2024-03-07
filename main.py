a1bd8091b16d9c55edae4cfe257ad71c78a32ebf
syedmuneeb321 <130203424+syedmuneeb321@users.noreply.github.com>
2024-03-07 17:51:24 +0500

    updated-app

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-st.title("test checker app")
+st.title("Paper Checker App")
 
 def get_gemini_response(input,image):
     model = genai.GenerativeModel('gemini-pro-vision')
